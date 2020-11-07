e0320c165096dcc86c2c9c9a00ade6b03e26f024
unknown <afaizi1999@gmail.com>
2020-11-07 10:05:40 +0700

    Update modules

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/afazzdev/app-say-hello
 
 go 1.15
 
-require github.com/afazzdev/go-say-hello v1.1.0
+require github.com/afazzdev/go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	gosayhello "github.com/afazzdev/go-say-hello"
+	gosayhello "github.com/afazzdev/go-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(gosayhello.SayHello())
+	fmt.Println(gosayhello.SayHello("broo!"))
 }
